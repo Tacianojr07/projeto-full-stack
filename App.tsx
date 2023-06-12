3757d395a98e5ab434662ad792fca47cd59c322d
Tacianojr07 <t.juniior13@gmail.com>
2023-06-12 14:25:09 -0300

    instalando native base e utilizando, criação compon

--- App.tsx
+++ App.tsx
@@ -1,23 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider,VStack,Text, Heading} from "native-base";
+import {useFonts, Roboto_400Regular,Roboto_500Medium, Roboto_700Bold} from "@expo-google-fonts/roboto"
+
+import {THEME} from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular,Roboto_500Medium,Roboto_700Bold})
+
+
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello Word!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+        <VStack flex={1} bgColor="gray.900" justifyContent="center" alignItems="center">
+          <Text color="white" fontSize="24" fontFamily="Heading" >
+            Hello Word!
+          </Text>
+          <StatusBar style="auto" />
+        </VStack>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff'
-  }
-});
