44cd7c519eee0c6361ef07f135ae754ea402ca14
Tacianojr07 <t.juniior13@gmail.com>
2023-06-20 09:04:00 -0300

    adicionando tipagem customizada

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { NativeBaseProvider,VStack,Text, StatusBar} from "native-base";
 import {useFonts, Roboto_400Regular,Roboto_500Medium, Roboto_700Bold} from "@expo-google-fonts/roboto"
 
-import { Signin } from "./src/screens/Signin";
+import { SignIn } from "./src/screens/Signin";
 import { Loading } from "./src/components/Loading";
 
 import {THEME} from './src/styles/theme';
@@ -19,7 +19,7 @@ export default function App() {
         translucent
         />
 
-        {fontsLoaded ? <Signin />: <Loading />}
+        {fontsLoaded ? <SignIn />: <Loading />}
     </NativeBaseProvider>
   );
 }
--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,12 +1,10 @@
 import { Center, Text } from "native-base";
+import Logo from '../assets/logo.svg'
 
-
-export function Signin() {
+export function SignIn() {
     return(
         <Center flex="1" bgColor="gray.900">
-            <Text fontSize="24" fontFamily="heading" color="white">
-                Signin
-            </Text>
+            <Logo />
         </Center>
     )
 }
