cf7f13e24bef1152dffbda383f7b92934eb9097c
Tacianojr07 <t.juniior13@gmail.com>
2023-06-30 09:20:13 -0300

    reaproveitando a estrutura NEW criando screen find

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import { Loading } from "./src/components/Loading";
 import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 import {THEME} from './src/styles/theme';
-import { New } from "./src/screens/New";
+import { Find } from "./src/screens/Find";
 
 
 
@@ -25,7 +25,7 @@ export default function App() {
         translucent
         />
       
-        {fontsLoaded ? <New />: <Loading />}
+        {fontsLoaded ? <Find />: <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
