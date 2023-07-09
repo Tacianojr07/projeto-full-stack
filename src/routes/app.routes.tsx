a09bf4ddf29cb5b5a003fc4a5854fb2ed110827e
Tacianojr07 <t.juniior13@gmail.com>
2023-07-09 09:58:50 -0300

    criando rota Pools na apllicação

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import { New } from '../screens/New';
+import { Pools } from '../screens/Pools';
 
 const { Navigator, Screen} = createBottomTabNavigator();
 
@@ -9,6 +10,10 @@ export function appRoutes() {
             <Screen 
             name="new"
             component={New}
+            />
+            <Screen 
+            name="pools"
+            component={Pools}
             />
         </Navigator>
     )
