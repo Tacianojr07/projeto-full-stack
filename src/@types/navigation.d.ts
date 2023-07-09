b50b8540e539aa76d0f01f56ea1566c0c47023d1
Tacianojr07 <t.juniior13@gmail.com>
2023-07-09 14:27:13 -0300

    corrigindo erro  na tipagem de rotas

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,6 +1,6 @@
 export declare global {
     namespace ReactNavigation {
-        interface RootParamsList {
+        interface RootParamList {
             new: undefined;
             pools:undefined;
             find: undefined;
