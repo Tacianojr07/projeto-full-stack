dfccb0238908dcf30a9a113164e37741090ac65a
Tacianojr07 <t.juniior13@gmail.com>
2023-07-09 12:57:19 -0300

    tipagens para rotas

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare globaç {
+    namespace ReactNavigation {
+        interface RootParamsList {
+            new: undefined;
+            pools:undefined;
+            find: undefined;
+            details: {
+                
+            }
+        }
+    }
+}
