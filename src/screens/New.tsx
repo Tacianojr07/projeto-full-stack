01e3e0a65042373ddf4b22a0563d2e57af23174b
Tacianojr07 <t.juniior13@gmail.com>
2023-07-10 09:39:09 -0300

    formatando documento

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,69 +1,64 @@
 import { useState } from "react";
 
-import { Heading, VStack,Input, Text, useToast } from "native-base";
-import {Header} from '../components/Header';
-import Logo from '../assets/logo.svg';
+import { Heading, VStack, Input, Text, useToast } from "native-base";
+import { Header } from "../components/Header";
+import Logo from "../assets/logo.svg";
 import { Button } from "../components/Button";
 
-
-
 export function New() {
+  const [title, setTitle] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
-    const [title, setTitle] = useState('');
-    const [isLoading,setLoading] = useState(false);
+  const toast = useToast();
 
-    const toast = useToast();
-
-    async function handlePoolCreate(){
-        if(!title.trim()) {
-            return toast.show({
-                title: 'Informe um nome para o bolão',
-                placement:'top',
-                bgColor: 'red.500'
-            })
-        };
-
-        try {
-
-            setLoading(true)
-
-        } catch (error) {
-             toast.show({
-             title: 'Informe um nome para o bolão',                placement:'top',
-              bgColor: 'red.500'
-            })
-        };
+  async function handlePoolCreate() {
+    if (!title.trim()) {
+      return toast.show({
+        title: "Informe um nome para o bolão",
+        placement: "top",
+        bgColor: "red.500",
+      });
     }
 
-    return(
-        <VStack flex={1} bgColor="gray.900">
-            <Header 
-            title="Criar novo bolão"
-            showBackButton
-            />
-            <VStack mt={8} mx={5} alignItems="center">
-                <Logo />
-                <Heading fontFamily="heading" color="white" fontSize="xl" my={8} textAlign="center"> 
-                    Crie seu prórpio bolão da copa e {'\n'} compatilhe entre amigos!
-                </Heading>
+    try {
+      setLoading(true);
+    } catch (error) {
+      toast.show({
+        title: "Informe um nome para o bolão",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
+  }
 
-                <Input 
-                mb={2}
-                placeholder="Qual nome do seu bolão"
-                onChangeText={setTitle}
-                value={title}
-                />
+  return (
+    <VStack flex={1} bgColor="gray.900">
+      <Header title="Criar novo bolão" showBackButton />
+      <VStack mt={8} mx={5} alignItems="center">
+        <Logo />
+        <Heading
+          fontFamily="heading"
+          color="white"
+          fontSize="xl"
+          my={8}
+          textAlign="center"
+        >
+          Crie seu prórpio bolão da copa e {"\n"} compatilhe entre amigos!
+        </Heading>
 
-                <Button 
-                title="CRIAR MEU BOLÃO"
-                isLoading={isLoading}
-                />
-                <Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4} >
-                    Após criar seu bolão, você receberá um código único
-                    que poderá usar para convidar outras pessoas
-                </Text>
-            </VStack>
+        <Input
+          mb={2}
+          placeholder="Qual nome do seu bolão"
+          onChangeText={setTitle}
+          value={title}
+        />
 
-        </VStack>
-    )
+        <Button title="CRIAR MEU BOLÃO" isLoading={isLoading} />
+        <Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4}>
+          Após criar seu bolão, você receberá um código único que poderá usar
+          para convidar outras pessoas
+        </Text>
+      </VStack>
+    </VStack>
+  );
 }
