723f0da68fddc35d129f2972dcbcecac96fe9084
Tacianojr07 <t.juniior13@gmail.com>
2023-07-10 09:38:28 -0300

    criando requisição para criação do bolão

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,10 +1,40 @@
-import { Heading, VStack,Input, Text } from "native-base";
+import { useState } from "react";
+
+import { Heading, VStack,Input, Text, useToast } from "native-base";
 import {Header} from '../components/Header';
 import Logo from '../assets/logo.svg';
 import { Button } from "../components/Button";
 
 
+
 export function New() {
+
+    const [title, setTitle] = useState('');
+    const [isLoading,setLoading] = useState(false);
+
+    const toast = useToast();
+
+    async function handlePoolCreate(){
+        if(!title.trim()) {
+            return toast.show({
+                title: 'Informe um nome para o bolão',
+                placement:'top',
+                bgColor: 'red.500'
+            })
+        };
+
+        try {
+
+            setLoading(true)
+
+        } catch (error) {
+             toast.show({
+             title: 'Informe um nome para o bolão',                placement:'top',
+              bgColor: 'red.500'
+            })
+        };
+    }
+
     return(
         <VStack flex={1} bgColor="gray.900">
             <Header 
@@ -20,10 +50,13 @@ export function New() {
                 <Input 
                 mb={2}
                 placeholder="Qual nome do seu bolão"
+                onChangeText={setTitle}
+                value={title}
                 />
 
                 <Button 
                 title="CRIAR MEU BOLÃO"
+                isLoading={isLoading}
                 />
                 <Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4} >
                     Após criar seu bolão, você receberá um código único
