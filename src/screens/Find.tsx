7a7347d7ff2a475107044a344eebeb1ea653ae6f
Tacianojr07 <t.juniior13@gmail.com>
2023-07-11 11:45:38 -0300

    habilitando busca por bolão por ID

--- src/screens/Find.tsx
+++ src/screens/Find.tsx
@@ -1,10 +1,37 @@
-import { Heading, VStack,Input, Text } from "native-base";
+import { Heading, VStack,Input, Text, useToast } from "native-base";
 import {Header} from '../components/Header';
 import Logo from '../assets/logo.svg';
 import { Button } from "../components/Button";
+import { api } from "../services/api";
+import { useState } from "react";
 
 
 export function Find() {
+    const [isLoading,setIsLoading] = useState(false);
+    const toast = useToast();   
+
+
+    async function handlePoolJoin() {
+        try {
+            setIsLoading(true)
+            const response = await api.get(`/pool/${isPool}`)
+        } catch (error) {
+            console.log(error)
+
+            if(error.response?.data?.message === 'Pool not found') { {
+                toast.show({
+                    title: 'Não foi possível encontrar o bolão',
+                    placement: 'top',
+                    bgColor: 'red.500'
+                })
+            }
+
+            }
+       
+        } finally {
+            setIsLoading(false)
+        }
+    }
     return(
         <VStack flex={1} bgColor="gray.900">
             <Header 
@@ -20,10 +47,13 @@ export function Find() {
                 <Input 
                 mb={2}
                 placeholder="Qual o código do bolão"
+                color="white"
                 />
 
                 <Button 
                 title="BUSCAR POR BOLÃO"
+                isLoading={isLoading}
+                onPress={handlePoolJoin}
                 />
             </VStack>
 
