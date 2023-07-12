c862d71f7c97910356dbad5781bd0e4546eac217
Tacianojr07 <t.juniior13@gmail.com>
2023-07-12 10:18:09 -0300

    criando requisição de detalhes do bolão

--- src/screens/details.tsx
+++ src/screens/details.tsx
@@ -1,20 +1,38 @@
+import { useState } from 'react';
+
 import { VStack } from  'native-base';
 import { useRoute } from '@react-navigation/native';
 
 import { Header } from '../components/Header';
+import { Loading } from '../components/Loading';
+
 
 interface RouteParams {
     id: string;
 }
 
 export function Details() {
+    const [isLoading, setIsLoading] = useState(true);
+
 
     const route = useRoute();
     const {id} = route.params as RouteParams;
 
+    async function fetchPoolDetails() {
+        try {
+            setIsLoading(true)
+        } catch (error) {
+            
+        } finally {
+            setIsLoading(false)
+        }
+    }
 
+    if(isLoading) {
+        return <Loading />
+    }
     return(
-        <VStack flex={1} bgColor="red.100">
+        <VStack flex={1} bgColor="gray.900">
             <Header title={  id } showBackButton showShareButton/>
         </VStack>
     );
