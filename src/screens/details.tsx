537cc33c9118a53bd7b70ad0d4b7368082d4e7c6
Tacianojr07 <t.juniior13@gmail.com>
2023-07-12 10:23:56 -0300

    mensagem de erro e sucesso

--- src/screens/details.tsx
+++ src/screens/details.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 
-import { VStack } from  'native-base';
+import { VStack, useToast } from  'native-base';
 import { useRoute } from '@react-navigation/native';
+
+import { api } from '../services/api';
 
 import { Header } from '../components/Header';
 import { Loading } from '../components/Loading';
@@ -12,19 +14,33 @@ interface RouteParams {
 }
 
 export function Details() {
-    const [isLoading, setIsLoading] = useState(true);
-
+    const [isLoading, setIsLoading] = useState(false);
+    const toast = useToast();
 
     const route = useRoute();
     const {id} = route.params as RouteParams;
 
     async function fetchPoolDetails() {
         try {
-            setIsLoading(true)
+            setIsLoading(true);
+
+            const response = await api.get(`/pools/${id}`);
+            console.log(response.data);
+
+
         } catch (error) {
-            
+            console.log(error);
+            toast.show({
+                title: 'Não foi possível carregar os detalhes do bolão',
+                placement: 'top',
+                bgColor: 'red.500'
+            });
+
+
+
+            throw error
         } finally {
-            setIsLoading(false)
+            setIsLoading(false);
         }
     }
 
