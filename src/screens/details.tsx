9ec9c9dec661bdd523f0f2d9cf85a1318be91145
Tacianojr07 <t.juniior13@gmail.com>
2023-07-12 09:52:01 -0300

    criando route details

--- src/screens/details.tsx
+++ src/screens/details.tsx
@@ -1,9 +1,9 @@
 import { VStack } from "native-base";
 
-export function details() {
+export function Details() {
     return(
         <VStack flex={1} bgColor="gray.900">
-            
+
         </VStack>
     );
 }
